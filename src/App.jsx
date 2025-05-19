845c1f0f17fa8bc9863cbff7a18a28b4ab831bb5
jashchavda <jashchavda18@gmail.com>
2025-05-19 11:57:25 +0530

    add

--- src/App.jsx
+++ src/App.jsx
@@ -11,11 +11,7 @@ import {
 import Auth from "./pages/auth";
 import Profile from "./pages/profile";
 import Chat from "./pages/Chat";
-
-import { useAppStore } from "../../store"; // ✅ adjust `..` based on location
-
-
-
+import { useAppStore } from "./store_temp";
 import { useState, useEffect } from "react";
 import { toast } from "sonner";
 import { GET_USER_INFO } from "./Services/urlHelper";
