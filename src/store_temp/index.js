dfead17c7466656711249c2682c8f54e001a8272
jashchavda <jashchavda18@gmail.com>
2025-05-19 11:59:12 +0530

    add

--- src/store_temp/index.js
+++ src/store_temp/index.js
@@ -1,6 +1,6 @@
 import { create } from 'zustand';
-import { createAuthSlice } from './Slice/AuthSlice';
-import { createChatSlice } from './Slice/ChatSlice';
+import { createAuthSlice } from './slice/AuthSlice';
+import { createChatSlice } from './slice/ChatSlice';
 
 export const useAppStore = create()((...a)=>({
   ...createAuthSlice(...a),
